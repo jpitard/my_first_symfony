64da404b6b38cd1077788945f0e8d82d4264a312
jpitard <johnny.pitard@gmail.com>
2017-01-12 09:03:56 +0100

    personnalisation des formulaires marque

--- src/AdminBundle/DataFixtures/ORM/LoadProductData.php
+++ src/AdminBundle/DataFixtures/ORM/LoadProductData.php
@@ -14,19 +14,24 @@ class LoadProductData extends AbstractFixture  implements OrderedFixtureInterfac
 
         for ($i=0; $i <= 10; $i++)
         {
+
             $product = new Product();
             $product->setTitle('testLoad'.$i);
             $product->setDescription('description test load'.$i);
             $product->setPrice(12+$i);
             $product->setQuantity(5+$i);
             $product->setMarque($this->getReference('brand'.$i));
+            for ($j=0; $j < 3; $j++){
 
+                $product->addCategory($this->getReference('category'.$j));
+
+            }
 
 
             $manager->persist($product);
             $manager->flush();
 
-            $this->addReference('produit'.$i, $product);
+            $this->addReference('product'.$i, $product);
 
 
         }
