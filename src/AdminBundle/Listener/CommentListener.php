53e557a02e8a75487d1e37a19b5d5bb77acb9616
jpitard <johnny.pitard@gmail.com>
2017-01-17 02:06:36 +0100

    debut dev commentaire produit

---
+++ src/AdminBundle/Listener/CommentListener.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: test
+ * Date: 12/01/2017
+ * Time: 09:49
+ */
+
+namespace AdminBundle\Listener;
+
+
+use AdminBundle\Entity\Comment;
+
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+
+class CommentListener
+{
+
+    public function prePersist(Comment $entity, LifecycleEventArgs $args)
+    {
+        $entity->setCreatedAT(new \DateTime());
+
+    }
+
+}
